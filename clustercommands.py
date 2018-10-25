c9b50861e4d52a3490ed620ffb1dcc18e9f854c1
wmc2tokyo <wmcooper2@yahoo.com>
2018-10-25 19:36:22 +0900

    subprocess.Popen() works on one pi, but need to get it working on the whole cluster.

--- clustercommands.py
+++ clustercommands.py
@@ -4,23 +4,39 @@
 #stand lib
 import subprocess
 
-
 def reboot(pi):
     """Reboot pi. Returns None."""
-    command = "ssh {0} && sudo reboot".format(pi)
-    subprocess.open(command)
+    command = "ssh {0} 'sudo reboot'".format(pi)
+    subprocess.Popen(command, shell=True)
     print("Rebooting {0}".format(pi))
 
-def cluster_reboot():
+def cluster_reboot(cluster):
     """Reboot the whole cluster. Returns None."""
     map(reboot, cluster)
 
-pi0 = ""
-pi1 = ""
-pi2 = ""
-pi3 = ""
+def ssh(pi):
+    """SSH into a pi. Returns None."""
+    command = "ssh {0}".format(pi)
+    subprocess.Popen(command, shell=True)
 
-cluster = [pi0, pi1, pi2, pi3]
+def shutdown(pi):
+    """Shutsdown a pi. Returns None."""
+    command = "ssh {0} 'sudo shutdown -h now'".format(pi)
+    subprocess.Popen(command, shell=True)
+    print("Shutting down {0}".format(pi))
+
+def cluster_shutdown():
+    """Shuts down the whole cluster. Returns None."""
+    map(shutdown, cluster)
 
 
+if __name__ == "__main__":
+    pi0 = ""
+    pi1 = ""
+    pi2 = ""
+    pi3 = ""
+#    cluster = [pi0, pi1, pi2, pi3]
+#    cluster_reboot(cluster)
+#    reboot(cluster[0])
+    shutdown(cluster[0])
 
