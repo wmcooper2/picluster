f9a5283b39c7d986edf77b4bf2fe4193aab9258c
wmc2tokyo <wmcooper2@yahoo.com>
2018-11-08 18:16:40 +0900

    Got parser to accept different arguments and reboot/shutdown commands work exclusively.

--- cluster.py
+++ cluster.py
@@ -8,8 +8,10 @@ import subprocess
 #custom
 from pi_ipaddresses import *
 
-pis         = [pi0, pi1, pi2, pi3]
-pi_outputs  = []
+cluster     = [pi0, pi1, pi2, pi3]      # ip addresses
+pi_outputs  = []                        # holds stdout from pis
+valid_args  = ["", "0", "1", "2", "3"]
+given_args  = []                        # holds args from command line
 
 def clear_terminal():
     """Clears the terminal window. Returns None."""
@@ -20,48 +22,59 @@ def show_outputs():
     for output in pi_outputs:
         print(str(pi_outputs.index(output)), "::", output)
 
-def _reboot(pis):
-    """Reboots all the nodes in pis. Returns None."""
+def valid(value):
+    """Determines validity. Returns Boolean."""
+    return value in valid_args
+
+def format_command(arg):
+    """Formats the command. Returns String."""
+    if valid(arg):
+        cmd = "pi"+arg
+        return cmd
+
+def _reboot(cluster):
+    """Reboots all the nodes in cluster. Returns None."""
     clear_terminal()
-    for pi in pis:
+    for pi in cluster:
         command = "ssh pi@"+pi+" 'sudo reboot'"
         print("command ::", command)
         c = subprocess.Popen(command, encoding='utf-8', 
                              stdout=subprocess.PIPE, shell=True)
         pi_outputs.append(c.stdout)
-        #call named pipe command
-        #save the output to a list with the pis name
-    #call the command that outputs to the mac's terminal
 
+# Main
 parser = ap.ArgumentParser(description="Commands for the pi-cluster.")
-
-# exclusive options
 group = parser.add_mutually_exclusive_group()
-#parser.add_argument('--nargs', nargs='+')
-group.add_argument("-r", "--reboot", help="Reboots the cluster.", nargs="+")
-group.add_argument("-s", "--shutdown", help="Shutdown the cluster.", 
-    action="store_true")
-group.add_argument("--rebootpi", help="Reboots a single pi.")
-group.add_argument("--shutdownpi", help="Shutdown a single pi.")
+group.add_argument("-r", "--reboot", help="Reboots the cluster.", 
+    nargs="?",const=["0", "1", "2", "3"])
+group.add_argument("-s", "--shutdown", help="Shuts down the cluster.",
+    nargs="?",const=["0", "1", "2", "3"])
+
+# not needed
+#group.add_argument("-s", "--shutdown", help="Shutdown the cluster.", 
+#    action="store_true")
+#group.add_argument("--rebootpi", help="Reboots a single pi.")
+#group.add_argument("--shutdownpi", help="Shutdown a single pi.")
 
 args = parser.parse_args()
 
-#if args.reboot:
-    #reboot
-#elif args.shutdown:
-    #shutdown
-for _, value in args._get_kwargs():
-    pi_outputs.append(value)
-#    if value is not None:
-#        print(value)
-#        pi_outputs.append(value)
+if args.reboot:
+#    for arg in given_args[0]:
+#        if arg!=None:
+#            print(format_command(arg))
+    print(args.reboot)
+elif args.shutdown:
+#    for arg in given_args[0]:
+#        if arg!=None:
+#            print(format_command(arg))
 
-# replace print statements with function calls for each pi
-if "0" in pi_outputs[0]:
-    print("found zero")
-if "1" in pi_outputs[0]:
-    print("found one")
-if "2" in pi_outputs[0]:
-    print("found two")
-if "3" in pi_outputs[0]:
-    print("found three")
+    print(args.shutdown)
+for _, value in args._get_kwargs():
+    given_args.append(value)
+    print(_, "::", value)
+
+#print("given_args::", given_args)
+
+#for arg in given_args[0]:
+#    if arg!=None:
+#        print(format_command(arg))
